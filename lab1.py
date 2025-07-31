da8b53707c92a75bd69b1a2e2c101324bd4d6c51
Rafat Alam <Rafat.Alam.RA@gmail.com>
2025-07-31 11:57:23 +0530

    update

--- lab1.py
+++ lab1.py
@@ -2,12 +2,14 @@ def get_grade():
   try:
     marks = input()
 
+    # Exiting code
     if marks.lower() == "exit":
       print("\nExiting...")
       return
 
     marks = int(marks)
 
+    # Condition Checks
     if marks < 0 or marks > 100:
       print("Invalid Input\n")
     elif marks >= 90:
@@ -21,6 +23,7 @@ def get_grade():
     else:
       print("Grade : F\n")
   except ValueError:
+    # Input unknown value
     print("Invalid Input\n")
 
 if __name__ == "__main__":
