b957ec72c2efe715aaf00465eecd85fb9f4ce0bd
Rafat Alam <126316989+rafat-alam@users.noreply.github.com>
2025-07-31 11:19:59 +0530

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,18 +1,27 @@
-# grade.py
-import sys
+def get_grade():
+  try:
+    marks = input()
 
-def get_grade(marks):
-    if marks >= 90:
-        return "A"
+    if marks.lower() == "exit":
+      print("\nExiting...")
+      return
+
+    marks = int(marks)
+
+    if marks < 0 or marks > 100:
+      print("Invalid Input\n")
+    elif marks >= 90:
+      print("Grade : A\n")
     elif marks >= 75:
-        return "B"
+      print("Grade : B\n")
     elif marks >= 60:
-        return "C"
+      print("Grade : C\n")
     elif marks >= 40:
-        return "D"
+      print("Grade : D\n")
     else:
-        return "F"
+      print("Grade : F\n")
+  except ValueError:
+    print("Invalid Input\n")
 
-if __name__ == "__main__":
-    marks = int(sys.stdin.read())
-    print(get_grade(marks))
+if _name_ == "_main_":
+  get_grade()
